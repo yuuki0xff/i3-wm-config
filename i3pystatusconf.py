eac7588ebc8ffa4b6cc24604ef4d89757433d48f
yuuki <yuuki0xff@gmail.com>
2019-07-20 18:59:56 +0900

    improve status bar
    * Change clock format
    * Reduce space for system load indicator
    * Show host name

--- i3pystatusconf.py
+++ i3pystatusconf.py
@@ -2,16 +2,17 @@ from i3pystatus import Status
 
 status = Status(standalone=True)
 
-# Displays clock like this:
-# Tue 30 Jul 11:59:46 PM KW31
 status.register("clock",
-    format="%a %-d %b %Y %X",)
+    format="%Y-%m-%d %H:%M %A")
 
 status.register("load",
-        format="Load {avg1} {avg5} {avg15}")
+        format="Load {avg1}")
 
 status.register("mem",
         format="MEM {avail_mem} MiB")
+
+status.register("shell",
+    command="hostname")
 
 status.register("xkblayout")
 
