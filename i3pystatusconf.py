12af9c765f63650ca1279f46682ef3ced1c20b69
yuuki <yuuki0xff@gmail.com>
2019-07-20 19:24:20 +0900

    show battery status

--- i3pystatusconf.py
+++ i3pystatusconf.py
@@ -1,9 +1,22 @@
+import glob
+import os.path
 from i3pystatus import Status
+
+def list_battery():
+    return [
+        os.path.basename(d)
+        for d in glob.glob("/sys/class/power_supply/BAT*")
+    ]
+
 
 status = Status(standalone=True)
 
 status.register("clock",
     format="%Y-%m-%d %H:%M %A")
+
+if list_battery():
+    status.register("battery",
+        format='{status} {percentage:.0f}%')
 
 status.register("load",
         format="Load {avg1}")
