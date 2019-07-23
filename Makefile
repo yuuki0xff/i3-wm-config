906ac0b472910eff3e90ed8ea25c07b2c7a94d4c
yuuki <yuuki0xff@gmail.com>
2019-07-23 14:59:46 +0900

    add template and rendering script

---
+++ Makefile
@@ -0,0 +1,14 @@
+all: build
+
+build:
+	$(MAKE) build-$(shell hostname)
+
+build-desktop1:
+	./render.py prefix=Mod4
+
+build-laptop2:
+	./render.py prefix=Mod4
+
+build-tagoken-desktop1:
+	./render.py prefix=Mod1
+
---
+++ render.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import sys
+import os
+import jinja2
+
+def main(args):
+	print('Loading options ...')
+	kv = {}
+	for a in args[1:]:
+		key, value = a.split('=')
+		kv[key] = value
+		print(f'  {key}={value}')
+
+	print('Rendering configuration from template ...')
+	env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
+	templ = env.get_template('config.tmpl')
+	config = templ.render(kv)
+
+	print('Writing to config file ...')
+	try:
+		os.unlink('config')
+	except FileNotFoundError:
+		pass
+	with open('config', 'w') as f:
+		f.write(config)
+	return 0
+
+if __name__ == '__main__':
+	exit(main(sys.argv))
